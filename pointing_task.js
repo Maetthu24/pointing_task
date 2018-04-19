d22977b662f2f67f7661e338197a8bfc14140191
Lukas Eisenring <lukas@icu.uzh.ch>
2018-04-19 10:56:23 +0200

    Add bottom buttons, adjust output, ...

--- pointing_task.js
+++ pointing_task.js
@@ -1,13 +1,29 @@
 // Select all buttons
 var buttons = document.querySelectorAll(".button");
 
-// Make a random button the first target
-var firstTargetIndex = generateRandomNumber(6, true);
-buttons[firstTargetIndex].classList.add("target");
+var experiment = [
+	[1,2,3,4,5,6],
+	[1,12,6,7],
+	[1,7,2,8,3,9]
+]
 
-var clicksText = "Timestamp,ClickX,ClickY\n";
+var participant = prompt("Enter Participant ID:", "0"); //id of the participant
+var condition = 0; //screen edge or not, out of input file
+var block = 0; // id of the sequence, starts always with 0
 
-var numberOfClicksNecessary = 10, numberOfClicks = 0;
+
+var clicksText = "Part;Cond;Block;Click;Tar_t;Click_t;PosX;PosY;Dist;Succ\n";
+
+var  numberOfClicks = 0;
+// Sequence of the targets
+var sequence = experiment[0];
+
+var numberOfClicksNecessary = sequence.length;
+
+var lastHit = getActualTime();
+
+// Highlite the first target
+hightlightNextTarget();
 
 // Add click listeners to all buttons
 for (var i = 0; i < buttons.length; i++) {
@@ -16,18 +32,36 @@ for (var i = 0; i < buttons.length; i++) {
 
 		// If the button is a target, a new target is selected
 		if(this.classList.contains("target")) {
-			var date = new Date();
-			clicksText += (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds());
-			clicksText += "," + event.clientX;
-			clicksText += "," + event.clientY;
+			var actualDate = getActualTime()
+            clicksText += participant;
+            clicksText += ";" + condition;
+            clicksText += ";" + block;
+            clicksText += ";" + numberOfClicks;
+			clicksText += ";" + actualDate;
+			clicksText += ";" + lastHit;
+			clicksText += ";" + event.clientX;
+			clicksText += ";" + event.clientY;
+            clicksText += ";" + "NaN";
+			clicksText += ";" + "1";
 			clicksText += "\n";
 			this.classList.remove("target");
+			lastHit = getActualTime();
 
-			if(numberOfClicks == numberOfClicksNecessary) {
-				saveTextAsFile(clicksText);
+			// Download File at the end of the programm
+			if(numberOfClicks >= experiment[block].length) {
+				if((block + 1) >= experiment.length){
+                    saveTextAsFile(clicksText);
+				}
+				else{
+					block += 1;
+					sequence = experiment[block];
+					numberOfClicks = 0;
+					numberOfClicksNecessary = sequence.length
+                    hightlightNextTarget();
+				}
+
 			} else {
-				highlightRandomButton(i + 1);
-				numberOfClicks += 1;
+				hightlightNextTarget();
 			}
 
 		}
@@ -54,6 +88,11 @@ function saveTextAsFile(textToSave) {
     downloadLink.click();
 }
 
+function getActualTime(){
+    var date = new Date();
+    return (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds());
+}
+
 // Copied from https://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
 function destroyClickedElement(event) {
     document.body.removeChild(event.target);
@@ -68,11 +107,23 @@ function highlightRandomButton(indexOfCurrentlySelectedButton) {
 		newIndex = generateRandomNumber(6, false);
 	}
 
-	var buttonId = "top" + newIndex;
+	var buttonId = "but" + newIndex;
 	var buttonToHighlight = document.querySelector("#" + buttonId);
 
 	buttonToHighlight.classList.add("target");
 }
+
+function hightlightNextTarget(){
+
+	var newIndex = sequence[numberOfClicks];
+
+    var buttonId = "but" + newIndex;
+    var buttonToHighlight = document.querySelector("#" + buttonId);
+
+    buttonToHighlight.classList.add("target");
+    numberOfClicks += 1;
+}
+
 
 function generateRandomNumber(max, fromZero) {
 
