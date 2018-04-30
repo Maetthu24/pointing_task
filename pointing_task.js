30cfdc66830eb2aa4128e8d0d5e1e9829d5589ca
aarauer <lukas.eisenring@uzh.ch>
2018-04-30 08:23:00 +0200

    add dummy training sequence

--- pointing_task.js
+++ pointing_task.js
@@ -3,6 +3,7 @@ var buttons = document.querySelectorAll('[id^=\'but\']');
 var startButton = document.querySelector('#but-start');
 
 var experiment = [
+	[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
     [1,11,6,16],
     [20,10,17,7],
 	[2,3,4,5],
